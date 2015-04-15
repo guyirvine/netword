1e6ce4c5a4d7018981c9dfbfcfb5bb2a7ca16b40
Guy Irvine <girvine@lic.co.nz>
2015-04-15 12:27:13 +1200

    Added New off Search

--- app.rb
+++ app.rb
@@ -4,75 +4,88 @@ require 'FluidDb'
 require 'json'
 
 class Netword < Sinatra::Application
+  before do
+  end
 
-before do
+  get '/word/:id' do
+    db = FluidDb::Db(ENV['DATABASE_URL'].sub('postgres', 'pgsql'))
+
+    c = params[:id]
+    sql = 'SELECT w.id, w.name, w.url FROM word_tbl w WHERE w.id = ?'
+    arr = db.queryForArray(sql, [c])
+
+    db.close
+
+    return arr.to_json
+  end
+
+  post '/word' do
+    db = FluidDb::Db(ENV['DATABASE_URL'].sub('postgres', 'pgsql'))
+
+    request.body.rewind
+    word = request.body.read
+
+    sql = 'INSERT INTO word_tbl( name ) VALUES ( ? )'
+    db.execute(sql, [word])
+
+    id = db.queryForValue("SELECT CURRVAL( 'word_seq' )")
+
+    db.close
+
+    return id
+  end
+
+  get '/search/:criteria' do
+    db = FluidDb::Db(ENV['DATABASE_URL'].sub('postgres', 'pgsql'))
+
+    c = "%#{params[:criteria]}%".upcase
+    sql = 'SELECT id, name, url FROM word_tbl WHERE UPPER(name) LIKE ?'
+    rst = db.queryForResultset(sql, [c])
+
+    db.close
+
+    return rst.to_json
+  end
+
+  get '/link/:parentid' do
+    db = FluidDb::Db(ENV['DATABASE_URL'].sub('postgres', 'pgsql'))
+
+    c = params[:parentid].to_i
+    sql = 'SELECT l1.word_1 AS id ' \
+          'FROM link_tbl l1 ' \
+          'WHERE l1.word_2 = ? ' \
+          'UNION ' \
+          'SELECT l2.word_2 AS id ' \
+          'FROM link_tbl l2 ' \
+          'WHERE l2.word_1 = ? ' \
+          ''
+
+    rst = db.queryForResultset(sql, [c, c])
+
+    db.close
+
+    return rst.to_json
+  end
+
+  get '/children/:parentid' do
+    db = FluidDb::Db(ENV['DATABASE_URL'].sub('postgres', 'pgsql'))
+
+    c = params[:parentid].to_i
+    sql = 'SELECT w1.name, w1.url ' \
+          'FROM word_tbl w1 ' \
+          '  INNER JOIN link_tbl l1 ON ( w1.id = l1.word_1 ) ' \
+          'WHERE l1.word_2 = ? ' \
+          'UNION ' \
+          'SELECT w2.name, w2.url ' \
+          'FROM word_tbl w2 ' \
+          '  INNER JOIN link_tbl l2 ON ( w2.id = l2.word_2 ) ' \
+          'WHERE l2.word_1 = ? ' \
+          ''
+
+    rst = db.queryForResultset(sql, [c, c])
+
+    db.close
+
+    return rst.to_json
+  end
 end
-
-get '/word/:id' do
-  db = FluidDb::Db(ENV['DATABASE_URL'].sub( "postgres", "pgsql" ))
-
-  c = params[:id]
-  sql = 'SELECT w.id, w.name, w.url FROM word_tbl w WHERE w.id = ?'
-  arr = db.queryForArray(sql, [c])
-
-  db.close
-
-  return arr.to_json
-end
-
-get '/search/:criteria' do
-  db = FluidDb::Db(ENV['DATABASE_URL'].sub( "postgres", "pgsql" ))
-
-  c = "%#{params[:criteria]}%".upcase
-  sql = 'SELECT id, name, url FROM word_tbl WHERE UPPER(name) LIKE ?'
-  rst = db.queryForResultset(sql, [c])
-
-  db.close
-
-  return rst.to_json
-end
-
-get '/link/:parentid' do
-  db = FluidDb::Db(ENV['DATABASE_URL'].sub( "postgres", "pgsql" ))
-
-  c = params[:parentid].to_i
-  sql = 'SELECT l1.word_1 AS id ' \
-        'FROM link_tbl l1 ' \
-        'WHERE l1.word_2 = ? ' \
-        'UNION ' \
-        'SELECT l2.word_2 AS id ' \
-        'FROM link_tbl l2 ' \
-        'WHERE l2.word_1 = ? ' \
-        ''
-
-  rst = db.queryForResultset(sql, [c, c])
-
-  db.close
-
-  return rst.to_json
-end
-
-get '/children/:parentid' do
-  db = FluidDb::Db(ENV['DATABASE_URL'].sub( "postgres", "pgsql" ))
-
-  c = params[:parentid].to_i
-  sql = 'SELECT w1.name, w1.url ' \
-        'FROM word_tbl w1 ' \
-        '  INNER JOIN link_tbl l1 ON ( w1.id = l1.word_1 ) ' \
-        'WHERE l1.word_2 = ? ' \
-        'UNION ' \
-        'SELECT w2.name, w2.url ' \
-        'FROM word_tbl w2 ' \
-        '  INNER JOIN link_tbl l2 ON ( w2.id = l2.word_2 ) ' \
-        'WHERE l2.word_1 = ? ' \
-        ''
-
-  rst = db.queryForResultset(sql, [c, c])
-
-  db.close
-
-  return rst.to_json
-end
-
-end
-
